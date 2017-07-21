ea9c48d85f2d140fbadb3dcf088ab8fd41a95ca6
Andrey <finix@list.ru>
2017-07-21 15:17:39 +0500

    checked in changes in index.html & folder and sub-folder js

--- brozcoding/sources/js/app.js
+++ brozcoding/sources/js/app.js
@@ -1,3 +1,15 @@
 function onSubmintFunc(){
-	alert("Your data have sent to Admin for approving!");
+	var win = window.open("", "Hi", "width=200,height=200");
+	win.document.write(document.getElementById("firstname").value + "<br>");
+	win.document.write(document.getElementById("secondname").value + "<br>");
+	win.document.write(document.getElementById("phonenum").value + "<br>");
+	win.document.write(document.getElementById("email").value + "<br>");
+}
+
+function onClickFunc(){
+	console.log(document.getElementById("firstname").value);
+	console.log(document.getElementById("secondname").value);
+	console.log(document.getElementById("phonenum").value);
+	console.log(document.getElementById("email").value);
+
 }
