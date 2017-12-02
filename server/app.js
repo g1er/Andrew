22e47a53c06698cf3ec8735266c97c19735e1076
Igor Kulishov <igorkulishov@gmail.com>
2017-12-02 13:31:26 -0500

    removed dependencies on server fraimworks and added instructions

--- server/app.js
+++ server/app.js
@@ -1,33 +1,37 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
+var http = require('http');
+var querystring = require('querystring');
+var url = require('url');
 var port = 4200;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-
-app.get('/api', function(req, res) {
-	console.log('received GET data: ', res.query);
-	res.end('Received your GET request!');
+var server = http.createServer().listen(port, function() {
+	console.log('server is listening on http://localhost:', port);
 });
-app.post('/api', function(req, res) {
-	res.write('<h1>Thank you for submitting:</h1></br>');
-	var submittedData = req.body;
-	for(var i in submittedData) {
-		res.write(  '<h2 style="color:blue"><i>' + i + ' : ' + submittedData[i] + '</i></h2>'+ '</br>');
-		console.log(i + ' : ' + submittedData[i]);
-	}
-	res.end(`
-		<button onclick="goBack()">Go Back</button>
 
-		<script>
-		function goBack() {
-		    window.history.back();
-		}
-		</script>`
-	);
-	});
-
-app.listen(port, function(err) {
-	console.log('running on http://localhost:', port, '/api');
+server.on('request', function (req, res) {
+    if (req.method == 'POST') {
+        var body = '';
+	    req.on('data', function (data) {
+	    	body += data;
+	    });
+	    req.on('end', function () {
+	        var post = querystring.parse(body);
+	        console.log(post);
+	        res.write('<h1>Server received POST message: </h1>');
+	        for(var i in post) {
+	        	res.write('<h2>' + i + ' : <i  style="color:blue">' + post[i] + '</i></h2>');
+	        }
+	        res.end(`
+	        	<h3>Click to go back</h3>
+				<button onclick="goBack()">Go Back</button>
+				<script>
+					function goBack() {
+					    window.history.back();
+					}
+				</script>`
+			);
+	    });
+    }
+    if (req.method == 'GET') {
+    	res.end('GET method');
+    }
 });
