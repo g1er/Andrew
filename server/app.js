59dd4e2e6756e0662c1722c5082650810fb227ee
Igor Kulishov <igorkulishov@gmail.com>
2017-12-02 13:34:06 -0500

    replaced files into server folder

--- server/app.js
+++ server/app.js
@@ -1,37 +1,15 @@
-var http = require('http');
-var querystring = require('querystring');
-var url = require('url');
-var port = 4200;
+function onSubmintFunc(){
+	var win = window.open("", "Hi", "width=200,height=200");
+	win.document.write(document.getElementById("firstname").value + "<br>");
+	win.document.write(document.getElementById("secondname").value + "<br>");
+	win.document.write(document.getElementById("phonenum").value + "<br>");
+	win.document.write(document.getElementById("email").value + "<br>");
+}
 
-var server = http.createServer().listen(port, function() {
-	console.log('server is listening on http://localhost:', port);
-});
+function onClickFunc(){
+	console.log(document.getElementById("firstname").value);
+	console.log(document.getElementById("secondname").value);
+	console.log(document.getElementById("phonenum").value);
+	console.log(document.getElementById("email").value);
 
-server.on('request', function (req, res) {
-    if (req.method == 'POST') {
-        var body = '';
-	    req.on('data', function (data) {
-	    	body += data;
-	    });
-	    req.on('end', function () {
-	        var post = querystring.parse(body);
-	        console.log(post);
-	        res.write('<h1>Server received POST message: </h1>');
-	        for(var i in post) {
-	        	res.write('<h2>' + i + ' : <i  style="color:blue">' + post[i] + '</i></h2>');
-	        }
-	        res.end(`
-	        	<h3>Click to go back</h3>
-				<button onclick="goBack()">Go Back</button>
-				<script>
-					function goBack() {
-					    window.history.back();
-					}
-				</script>`
-			);
-	    });
-    }
-    if (req.method == 'GET') {
-    	res.end('GET method');
-    }
-});
+}
