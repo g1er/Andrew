2062b73fd167edee8f0c13e8c3ea1d577b6979f9
Andrey <finix@list.ru>
2018-07-11 00:00:29 +0500

    ngIf else practice

--- angular/develop-andrey/src/app/app.module.ts
+++ angular/develop-andrey/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SwmComponent } from './swm/swm.component';
@@ -10,7 +11,8 @@ import { SwmComponent } from './swm/swm.component';
     SwmComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular/develop-andrey/src/app/swm/swm.component.ts
+++ angular/develop-andrey/src/app/swm/swm.component.ts
@@ -7,15 +7,23 @@ import { Component } from '@angular/core';
 })
 export class SwmComponent {
 
-  addText = "";
-  addNewText(){
-    this.addText = "Let competitions begin!";
-  }
+  // addText = "";
+  // addNewText(){
+  //   this.addText = "Let competitions begin!";
+  // }
 
-  textFieldValue = "";
-  onKeyUp(event){
-    this.textFieldValue = event.target.value
-  }
+  // textFieldValue = "";
+  // onKeyUp(event){
+  //   this.textFieldValue = event.target.value
+  // }
 
+  swimStyle = "";
+  swimStyleStatus: boolean=false;
+
+  chosenStyle() {
+    this.swimStyleStatus = true;
+    this.swimStyle = (<HTMLInputElement>document.getElementById("styleLine")).value;
+    if (this.swimStyle == "") this.swimStyleStatus = false;
+  }
 
 }
