7c01f2a182d58db368ec3357fb7ff66512fd8402
Andrey <finix@list.ru>
2018-08-12 21:26:23 +0500

    added points in directives

--- angular/develop-andrey/src/app/directives/color.directive.ts
+++ angular/develop-andrey/src/app/directives/color.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, OnInit, ElementRef, Renderer2 } from "@angular/core";
+import { Directive, OnInit, ElementRef, Renderer2, HostListener, HostBinding } from "@angular/core";
 
 @Directive({
     selector: '[appColor]'
@@ -7,7 +7,17 @@ import { Directive, OnInit, ElementRef, Renderer2 } from "@angular/core";
 export class colorDirective implements OnInit {
     constructor (private elRef: ElementRef, private renderer: Renderer2) {}
 
+    @HostBinding('style.backgroundColor') bcolor;
     ngOnInit () {
-        this.renderer.setStyle(this.elRef.nativeElement, "color", "#5980c1");
+        this.renderer.setStyle(this.elRef.nativeElement, "color", "#541126");
+        this.renderer.setStyle(this.elRef.nativeElement, "display", "inline-block");
+
+    }
+
+    @HostListener('mouseenter') mouseEnter(){
+        this.bcolor = '#abc';
+    }
+    @HostListener('mouseleave') mouseLeave(){
+        this.bcolor = 'transparent';
     }
 }
